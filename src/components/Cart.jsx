5a43a3fd297f62197e72307e0f0d293310ed0b61
pauzuffi <paula.zuffinetti@gmail.com>
2024-09-05 11:47:07 -0300

    agregado de collections y productos

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,53 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ItemsContext } from "../contexts/ItemsContext";
 import Container from 'react-bootstrap/Container';
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
+const initialValues = {
+    phone: "",
+    email: "",
+    name: "",
+};
 
 export const Cart = () => {
-    const {items, removeItem, reset} = useContext(ItemsContext);
+    const [buyer, setBuyer] = useState(initialValues);
+
+    const { items, removeItem, reset } = useContext(ItemsContext);
+
+    const handleChange = (ev) => {
+        setBuyer((prev) => {
+            return {
+                ...prev,
+                [ev.target.name]: ev.target,value,
+            };
+        });
+    };
+
+
+    const total = items.reduce((acc, act) => acc + act.price * act.quantity, 0);
+
+    const sendOrder = () => {
+        const order = {
+            buyer,
+            items,
+            total
+        };
+
+        const db = getFirestore();
+        const orderCollection = collection(db, "orders");
+
+        addDoc(orderCollection, order).then(({ id }) => {
+            if (id) {
+                alert("Su orden: " + id + "ha sido completada!");
+            }
+        })
+        .finally(() => {
+            reset();
+            setBuyer(initialValues);
+        });
+    };
+
+    if(items.length === 0) return "VE A LA HOME";
 
     return (
     <Container>
@@ -19,6 +62,29 @@ export const Cart = () => {
                 </div>
             );
         })}
+        <br />
+        <div>Total $ {total}</div>
+        <br/>
+        <form>
+            <div>
+                <label>Nombre</label>
+                <input value={buyer.name} name="name" onChange={handleChange} />
+            </div>
+            <div>
+                <label>Teléfono</label>
+                <input value={buyer.phone} name="phone" onChange={handleChange} />
+            </div>
+            <div>
+                <label>Email</label>
+                <input value={buyer.email} name="email" onChange={handleChange} />
+            </div>
+            <button type="button" onClick={sendOrder}>
+                Comprar
+            </button>
+        </form>
+
+
+
     </Container>
     );
 }
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,17 @@
-import cart from '../assets/cart.png'
-import { Link } from 'react-router-dom'
+import cart from '../assets/cart.png';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { ItemsContext } from '../contexts/ItemsContext';
+
 
 export const CartWidget = () => {
+    const { items } = useContext(ItemsContext);
+
+    const quantity = items.reduce((acc, act) => acc+act.quantity, 0)
     return (
         <Link to="/Cart">
             <img src={cart} height={25}/>
-            <span>4</span>
+            <span>{quantity}</span>
         </Link>
 
 
